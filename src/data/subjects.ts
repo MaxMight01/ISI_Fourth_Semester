5ec8d84dfb191f791979187ff5e89b4afee1da26
ramdassingh399@gmail.com <ramdassingh399@gmail.com>
2026-01-04 16:05:01 +0530

    pdf path fix

--- src/data/subjects.ts
+++ src/data/subjects.ts
@@ -1,3 +1,9 @@
+import elecPdf from '../../subjects/Electrodynamics/elec.pdf';
+import ramPdf from '../../subjects/Rings_and_Modules/ram.pdf';
+import itlmarPdf from '../../subjects/Introduction_to_Linear_Models_and_Regression/itlmar.pdf';
+import odePdf from '../../subjects/Ordinary_Differential_Equations/ode.pdf';
+import topoPdf from '../../subjects/Topology/topo.pdf';
+
 export interface Subject {
   id: string;
   name: string;
@@ -11,35 +17,35 @@ export const SUBJECTS: Subject[] = [
     id: 'electrodynamics',
     name: 'Electrodynamics',
     shortCode: 'ELEC',
-    pdfPath: '/subjects/Electrodynamics/elec.pdf',
+    pdfPath: elecPdf,
     courseInfoUrl: 'https://www.isibang.ac.in/~adean/infsys/database/Bmath/Ele.html',
   },
   {
     id: 'rings-modules',
     name: 'Rings and Modules',
     shortCode: 'RAM',
-    pdfPath: '/subjects/Rings_and_Modules/ram.pdf',
+    pdfPath: ramPdf,
     courseInfoUrl: 'https://www.isibang.ac.in/~adean/infsys/database/Bmath/RM.html',
   },
   {
     id: 'linear-models-regression',
     name: 'Introduction to Linear Models and Regression',
     shortCode: 'LMR',
-    pdfPath: '/subjects/Introduction_to_Linear_Models_and_Regression/itlmar.pdf',
+    pdfPath: itlmarPdf,
     courseInfoUrl: 'https://www.isibang.ac.in/~adean/infsys/database/Bmath/LMR.html',
   },
   {
     id: 'ordinary-differential-equations',
     name: 'Ordinary Differential Equations',
     shortCode: 'ODE',
-    pdfPath: '/subjects/Ordinary_Differential_Equations/ode.pdf',
+    pdfPath: odePdf,
     courseInfoUrl: 'https://www.isibang.ac.in/~adean/infsys/database/Bmath/ODE.html',
   },
   {
     id: 'topology',
     name: 'Topology',
     shortCode: 'TOPO',
-    pdfPath: '/subjects/Topology/topo.pdf',
+    pdfPath: topoPdf,
     courseInfoUrl: 'https://www.isibang.ac.in/~adean/infsys/database/Bmath/Top.html',
   },
 ];
